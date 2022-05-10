6129a396720dcb2b452b0151e083ca85ee036c4b
Bill Ray <bill.ray0259@gmail.com>
2022-05-10 15:19:40 -0400

    attempting to get decoder to train

--- src/lib/decoder_dataset.py
+++ src/lib/decoder_dataset.py
@@ -13,7 +13,15 @@ from src.lib.style_classifier import StyleEncoder
 
 class DecoderDataset(Dataset):
 
-    def __init__(self, df, batch_size=64):
+    def __init__(self, df=None, batch_size=64, state_dict=None):
+        if state_dict is not None:
+            self.df = state_dict["df"]
+            self.style_embeds = state_dict["style_embeds"]
+            self.para_token_embeds = state_dict["para_token_embeds"]
+            self.para_attn_mask = state_dict["para_attn_mask"]
+            self.text_token_ids = state_dict["text_token_ids"]
+            return
+        
         self.df = df
         
         style_encoder = StyleEncoder()
@@ -38,8 +46,14 @@ class DecoderDataset(Dataset):
         # concat token embeddings for all texts
         self.para_token_embeds = torch.cat(self.para_token_embeds, dim=0) # token embeddings of the paraphrases in the same order as in df
         del paraphraser
-        torch.cuda.empty_cache()
 
+        # put everything on the cpu
+        self.style_embeds = self.style_embeds.to("cpu")
+        self.para_token_embeds = self.para_token_embeds.to("cpu")
+        self.para_attn_mask = self.para_attn_mask.to("cpu")
+        self.text_token_ids = self.text_token_ids.to("cpu")
+
+        torch.cuda.empty_cache()
         
     def __len__(self):
         return len(self.df)
@@ -56,3 +70,20 @@ class DecoderDataset(Dataset):
         text_token_ids = self.text_token_ids[idx]
 
         return (style_embed, para_token_embed, para_attn_mask), text_token_ids
+    
+
+    def save_state_dict(self, path):
+        state = {
+            "df": self.df,
+            "style_embeds": self.style_embeds,
+            "para_token_embeds": self.para_token_embeds,
+            "para_attn_mask": self.para_attn_mask,
+            "text_token_ids": self.text_token_ids
+        }
+        torch.save(state, path)
+    
+
+    @classmethod
+    def from_state_dict(cls, path):
+        state = torch.load(path)
+        return cls(state_dict=state)
